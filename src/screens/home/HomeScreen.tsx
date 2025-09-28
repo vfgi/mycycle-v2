c964ad586692de37061c4193ab7bb0715bc98321
Vitor Fernandes <vfgi@hotmail.com>
2025-09-28 01:05:08 +0100

    fix header size and ptpt translations

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,54 +1,21 @@
 import React from "react";
 import { ScrollView } from "react-native";
-import { VStack, Text } from "@gluestack-ui/themed";
-import { useAuth } from "../../contexts/AuthContext";
-import { FIXED_COLORS } from "../../theme/colors";
-import { useTranslation } from "../../hooks/useTranslation";
+import { VStack } from "@gluestack-ui/themed";
 import { SafeContainer, StatsCard } from "../../components";
 
 export const HomeScreen: React.FC = () => {
-  const { user } = useAuth();
-  const { t } = useTranslation();
-
   return (
-    <SafeContainer paddingHorizontal={0}>
-      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+    <SafeContainer paddingHorizontal={0} paddingTop={0}>
+      <ScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
+        showsVerticalScrollIndicator={false}
+        bounces={true}
+        scrollEventThrottle={16}
+      >
         <VStack flex={1} space="lg">
           {/* Stats Card no topo */}
           <StatsCard />
-
-          {/* Conteúdo existente */}
-          <VStack justifyContent="center" alignItems="center" p="$6">
-            <VStack space="lg" alignItems="center" w="$full">
-              <Text
-                color={FIXED_COLORS.text[50]}
-                fontSize="$2xl"
-                fontWeight="$bold"
-                textAlign="center"
-              >
-                {t("home.welcome")}
-              </Text>
-
-              {user && (
-                <VStack space="sm" alignItems="center">
-                  <Text
-                    color={FIXED_COLORS.text[50]}
-                    fontSize="$lg"
-                    textAlign="center"
-                  >
-                    {t("home.hello")} {user.name}
-                  </Text>
-                  <Text
-                    color={FIXED_COLORS.text[50]}
-                    fontSize="$sm"
-                    textAlign="center"
-                  >
-                    {user.email}
-                  </Text>
-                </VStack>
-              )}
-            </VStack>
-          </VStack>
         </VStack>
       </ScrollView>
     </SafeContainer>
